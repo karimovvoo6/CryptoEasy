74e16401a71749c98ec840dce0d56337386893d5
Karimov Dalerkhuja <karimovdalerkhuja@gmail.com>
2025-04-12 15:59:02 +0500

    Start

---
+++ src/app/shared/components/ui-buttons/ui-buttons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-buttons',
+  imports: [],
+  templateUrl: './ui-buttons.component.html',
+  styleUrl: './ui-buttons.component.scss'
+})
+export class UiButtonsComponent {
+
+}
